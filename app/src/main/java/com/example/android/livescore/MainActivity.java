1df7dd3b6758c37cbd5e5a3f2261412de5e71e38
Amedeo <amedeo.fanelli@gmail.com>
2018-02-18 21:28:06 +0100

    Updated version

--- app/src/main/java/com/example/android/livescore/MainActivity.java
+++ app/src/main/java/com/example/android/livescore/MainActivity.java
@@ -21,89 +21,91 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-    *This method is called when button with Manchester City logo is clicked.
-    */
-    public void displayScoreForManCity (int score) {
+     * These methods are called to display the goals of both teams.
+     */
+    public void displayScoreForManCity(int score) {
         TextView scoreView = (TextView) findViewById(R.id.manCity_score);
         scoreView.setText(String.valueOf(score));
     }
-    /*Display score of Manchester City.
-     */
-    public void oneGoalManCity (View view) {
-        scoreManCity = scoreManCity + 1;
-        displayScoreForManCity (scoreManCity);
-    }
 
-    /**
-     *This method is called when button with Chelsea logo is clicked.
-     */
-    public void displayScoreForChelsea (int score) {
+    public void displayScoreForChelsea(int score) {
         TextView scoreView = (TextView) findViewById(R.id.chelsea_score);
         scoreView.setText(String.valueOf(score));
     }
 
-    /*Display score of Chelsea.
+    /*These methods are called to add one goal.
      */
-
-    public void oneGoalChelsea (View view) {
-        scoreChelsea = scoreChelsea + 1;
-        displayScoreForChelsea (scoreChelsea);
+    public void oneGoalManCity(View view) {
+        scoreManCity = scoreManCity + 1;
+        displayScoreForManCity(scoreManCity);
     }
+
+    public void oneGoalChelsea(View view) {
+        scoreChelsea = scoreChelsea + 1;
+        displayScoreForChelsea(scoreChelsea);
+    }
+
     /**
-     *This method is called when yellowcard for Manchester City is clicked.
+     * Display yellow/red cards for Manchester city
      */
-    public void displayYellowForManCity (int score) {
+    public void displayYellowForManCity(int score) {
         TextView scoreView = (TextView) findViewById(R.id.manCity_yellowCard);
         scoreView.setText(String.valueOf(score));
     }
-    /*Display score of Manchester City.
-     */
-    public void oneYellowManCity (View view) {
+
+    public void oneYellowManCity(View view) {
         yellowManCity = yellowManCity + 1;
-        displayYellowForManCity (yellowManCity);
-    }
-    public void displayYellowForChelsea (int score) {
-        TextView scoreView = (TextView) findViewById(R.id.chelsea_yellowCard);
-        scoreView.setText(String.valueOf(score));
-    }
-    public void oneYellowChelsea (View view) {
-        yellowChelsea = yellowChelsea + 1;
-        displayYellowForChelsea (yellowChelsea);
+        displayYellowForManCity(yellowManCity);
     }
 
-    /*Display score of Manchester City.
-     */
-
-    public void displayRedForManCity (int score) {
+    public void displayRedForManCity(int score) {
         TextView scoreView = (TextView) findViewById(R.id.manCity_redCard);
         scoreView.setText(String.valueOf(score));
     }
-    public void oneRedManCity (View view) {
+
+    public void oneRedManCity(View view) {
         redManCity = redManCity + 1;
-        displayRedForManCity (redManCity);
+        displayRedForManCity(redManCity);
     }
 
-    public void displayRedForChelsea (int score) {
+    /**
+     * Display yellow/red cards for Chelsea
+     */
+    public void displayYellowForChelsea(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.chelsea_yellowCard);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void oneYellowChelsea(View view) {
+        yellowChelsea = yellowChelsea + 1;
+        displayYellowForChelsea(yellowChelsea);
+    }
+
+    public void displayRedForChelsea(int score) {
         TextView scoreView = (TextView) findViewById(R.id.chelsea_redCard);
         scoreView.setText(String.valueOf(score));
     }
-    public void oneRedChelsea (View view) {
+
+    public void oneRedChelsea(View view) {
         redChelsea = redChelsea + 1;
-        displayRedForChelsea (redChelsea);
+        displayRedForChelsea(redChelsea);
     }
 
-    public void resetAll (View view){
-    scoreManCity = 0;
-    scoreChelsea = 0;
-    yellowManCity = 0;
-    yellowChelsea = 0;
-    redManCity = 0;
-    redChelsea = 0;
-    displayScoreForManCity (scoreManCity);
-    displayScoreForChelsea (scoreChelsea);
-    displayYellowForManCity (yellowManCity);
-    displayYellowForChelsea (yellowChelsea);
-    displayRedForManCity (redManCity);
-    displayRedForChelsea (redChelsea);
+    /**
+     * Method to reset everything
+     */
+    public void resetAll(View view) {
+        scoreManCity = 0;
+        scoreChelsea = 0;
+        yellowManCity = 0;
+        yellowChelsea = 0;
+        redManCity = 0;
+        redChelsea = 0;
+        displayScoreForManCity(scoreManCity);
+        displayScoreForChelsea(scoreChelsea);
+        displayYellowForManCity(yellowManCity);
+        displayYellowForChelsea(yellowChelsea);
+        displayRedForManCity(redManCity);
+        displayRedForChelsea(redChelsea);
     }
 }
